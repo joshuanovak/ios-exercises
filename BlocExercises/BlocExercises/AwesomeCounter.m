811e2ed9d7b99941002f8a9d4fd896eed8afa13b
Joshua Novak <josh19novak@gmail.com>
2015-07-13 22:20:12 -0500

    Checkpoint 12

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,26 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSMutableString *actual = [[NSMutableString alloc] initWithString:@""];
+    
+    if (number<otherNumber) {
+        while (number<otherNumber+1) {
+            [actual appendString:[NSString stringWithFormat:@"%d",(int)number]];
+            number++;
+        }
+        return actual;
+    }else if(number>otherNumber){
+        while (otherNumber<number+1) {
+            [actual appendString:[NSString stringWithFormat:@"%d",(int)otherNumber]];
+            otherNumber++;
+        }
+        return actual;
+    }else{
+        [actual appendString:[NSString stringWithFormat:@"%d",(int)number]];
+        return actual;
+    }
+    
 }
 
 @end
